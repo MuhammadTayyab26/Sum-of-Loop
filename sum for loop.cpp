5fbf5b0e0e4deca02305d376e65aeb382c6b41a0
MuhammadTayyab26 <bcsm-f16-526@superior.edu.pk>
2021-02-15 01:58:41 +0500

    How numbers are added while in a loop

---
+++ sum for loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	for(n=1;n<=5;n++)
+	{
+		sum=sum+n;
+	}
+cout<<"sum ="<<sum<<endl;
+
+getch();
+return 0;
+}
+
